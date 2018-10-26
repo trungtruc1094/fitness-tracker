dd1d4aa049570c658d873ca35747e2c766e35513
trungtrucnguyen <trungtruc1094@gmail.com>
2018-10-27 03:15:31 +1300

    Initial commit

---
+++ src/app/material.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from "@angular/core";
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule
+} from "@angular/material";
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule
+  ]
+})
+export class MaterialModule {}
